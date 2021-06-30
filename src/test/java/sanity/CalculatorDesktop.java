f0371448fdfa0388968118457bcf0aae02a97dfc
OxanaDrezner <oxana.drezner@gmail.com>
2021-06-30 12:37:05 +0300

    Exporting data from Desktop Test Case to Data Configuration xml file

--- src/test/java/sanity/CalculatorDesktop.java
+++ src/test/java/sanity/CalculatorDesktop.java
@@ -14,7 +14,7 @@ public class CalculatorDesktop extends CommonOps {
     @Test(description = "Test01 - Verify Addition Command")
     @Description("This test sums numbers and Verifies the Result")
     public void Test01_verifyAdditionCommand(){
-        DesktopFlows.calculateAddition(1,8);
+        DesktopFlows.calculateAddition(Integer.parseInt(getData("FirstNumber")),Integer.parseInt(getData("SecondNumber")));
         Verifications.assertTextInElement(calcMain.getField_result(),"Display is 9");
     }
 
